9cb11abd6b2eb81eae1db76bb69e5c6b9b49a14a
c-adkins <adkins.carey@gmail.com>
2020-12-26 09:06:46 -0800

    complete loops and iterators exercises

---
+++ loops_iterators/recursion_zero.rb
@@ -0,0 +1,9 @@
+def count_to_zero(start)
+  puts start
+  if start > 0
+    count_to_zero(start - 1)
+  end
+end
+
+count_to_zero(10)
+count_to_zero(-4)
---
+++ loops_iterators/stop.rb
@@ -0,0 +1,7 @@
+loop do
+  puts "Type STOP to stop. Or anything else to continue"
+  answer = gets.chomp
+  if answer == 'STOP'
+    break
+  end
+end
